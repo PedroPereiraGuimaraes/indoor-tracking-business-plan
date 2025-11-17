896069aae41db5356ee9ff54937567304ac3b238
PedroPereiraGuimaraes <pedropeguimaraes@gmail.com>
2025-11-17 19:00:10 -0300

    feat: add @tailwindcss/postcss to dependencies and update PostCSS configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Carrega as variáveis de ambiente do arquivo .env localmente
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), "");
 
   return {
     plugins: [react()],
+    css: {
+      postcss: "./postcss.config.js",
+    },
     define: {
       // Isso permite usar process.env.API_KEY no código React, mesmo rodando no Vite
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
-    }
-  }
-})
+      "process.env.API_KEY": JSON.stringify(env.API_KEY),
+    },
+  };
+});
